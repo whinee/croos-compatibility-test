9b24a0d9b383ab9c8ef24efcb26d281bbceab4cc
whine <81570596+whinee@users.noreply.github.com>
2022-04-13 20:45:12 +0800

    Update win_test.py

--- win_test.py
+++ win_test.py
@@ -1,7 +1,7 @@
 import win32api
 
 for drive in win32api.GetLogicalDriveStrings().split("\x00"):
-		type = win32file.GetDriveType(drive)
-    print(type)
-		if type == win32con.DRIVE_FIXED:
-			print(win32api.GetVolumeInformation(drive))
+	type = win32file.GetDriveType(drive)
+   	print(type)
+	if type == win32con.DRIVE_FIXED:
+		print(win32api.GetVolumeInformation(drive))
