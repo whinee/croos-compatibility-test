5f07ad92965802a166d27ad1ffd00db4dece4556
whi~nyaan! <whinyaan@gmail.com>
2023-02-07 23:25:26 +0800

    p

--- rmm.py
+++ rmm.py
@@ -3,6 +3,10 @@ import re
 with open("os") as f:
     os = f.read()
 with open("README.md") as f:
-    op=re.sub(f"(```{os}\n)(.+?)(?=```)",rf"\1{sys.stdin.read()}".rstrip() + "\n",f.read(),0,re.MULTILINE|re.DOTALL)
+    op=re.sub(
+        f"(```{os.strip()}\n)(.+?)(?=```)",
+        rf"\1{sys.stdin.read()}".rstrip() + "\n",
+        f.read(),
+        0,re.MULTILINE | re.DOTALL)
 with open("README.md", "w") as f:
     f.write(op)
